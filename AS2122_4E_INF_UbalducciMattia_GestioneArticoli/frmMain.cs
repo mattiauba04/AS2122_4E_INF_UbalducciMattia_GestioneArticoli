7850f91f03a8024b43b57e612275a6ef0692f184
PC10 <PC10@INFO5-5381>
2022-05-18 09:12:30 +0200

    Aggiungere i file di progetto.

---
+++ AS2122_4E_INF_UbalducciMattia_GestioneArticoli/frmMain.cs
@@ -0,0 +1,10 @@
+namespace AS2122_4E_INF_UbalducciMattia_GestioneArticoli
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
